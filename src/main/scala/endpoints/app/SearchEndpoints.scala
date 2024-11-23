838d6d8504cb2e6dfdccd5f2b2a995bc8a952652
Diego (PC sobremesa) <dcarh26@gmail.com>
2024-11-23 12:17:15 +0100

    Comentario quitado

--- src/main/scala/endpoints/app/SearchEndpoints.scala
+++ src/main/scala/endpoints/app/SearchEndpoints.scala
@@ -15,21 +15,6 @@ object SearchEndpoints {
   private val searchBaseEndpoint: PublicEndpoint[Unit, Unit, Unit, Any] =
     endpoint.in("api" / "search")
 
-  // val searchEndpoint: PublicEndpoint[(Option[String], String, Option[String]), Unit, MediaContentList, Any] =
-  //   searchBaseEndpoint
-  //     .name("Search endpoint")
-  //     .description("This endpoint searches any element on the app based on text coincidence")
-  //     .get
-  //     .in(QueryInputs.querySortBy)
-  //     .in(QueryInputs.querySearch)
-  //     .in(QueryInputs.queryCategories)
-  //     .out(SocialOutputs.jsonMediaContentListOut)
-
-  // TODO: Esto está mal pensado. El endpoint de arriba no devuelve cualquier elemeneto de la App, sino una
-  // TODO: MediaContentList. Habría que descomentar los métodos de abajo, que son más apropiados, o crear un tipo que
-  // TODO: englobe todo lo que puede devolver este endpoint y que la búsqueda no sea específica de un solo tipo de
-  // TODO: elemento.
-
   val searchMovieEndpoint: PublicEndpoint[(String, Option[String]), Unit, List[Movie], Any] =
     searchBaseEndpoint
       .name("Search movie endpoint")
