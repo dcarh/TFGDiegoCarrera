47d45d42e81fde65ed5e8dc20699adad4d26b721
Diego (PC sobremesa) <dcarh26@gmail.com>
2025-06-19 22:55:52 +0200

    Fix main

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -6,13 +6,10 @@ import routes.HttpRoutes.httpRoutes
 object Main extends IOApp {
 
   override def run(args: List[String]): IO[ExitCode] = {
-
-    SwaggerInterpreter
-
+    
     BlazeServerBuilder[IO]
       .bindHttp(8080, "localhost")
       .withHttpApp(httpRoutes.orNotFound)
-      .
       .serve
       .compile
       .drain
